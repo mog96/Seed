e501807895e019cf870c792562d28596228d089d
Mateo Garcia <mateog@stanford.edu>
2017-06-21 09:01:49 -0700

    Screw it, static init.

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -115,18 +115,14 @@ quizApp.controller('QuizController', ['$scope', '$window', '$interval', '$routeP
         ]
       }
     ];
-    $scope.quiz.attemptsPerQuestion = new Array($scope.quiz.pages.length);
     $scope.quiz.numQuestions = 0;
-    var i;
-    for (i = 0; i < $scope.quiz.pages.length; i++) {
-      var j;
-      $scope.quiz.attemptsPerQuestion[i] = new Array($scope.quiz.pages[i].questions.length, 0);
-      for (j = 0; j < $scope.quiz.pages[i].questions.length; j++) {
+    for (var i = 0; i < $scope.quiz.pages.length; i++) {
+      for (var j = 0; j < $scope.quiz.pages[i].questions.length; j++) {
         $scope.quiz.numQuestions += 1;
       }
     }
-    console.log("ATTEMPTS INIT", $scope.quiz.attemptsPerQuestion);
     $scope.quiz.solutions = [[5], [2], [1, 4], [5, 2]];  // Indices of correct answers, ordered by quiz page/question number.
+    $scope.quiz.attemptsPerQuestion = [[0], [0], [0, 0], [0, 0]];
     $scope.quiz.selectedAnswers = [];
     $scope.quiz.continue = false;
     $scope.quiz.mistake = false;
@@ -175,9 +171,10 @@ quizApp.controller('QuizController', ['$scope', '$window', '$interval', '$routeP
     }
 
     $scope.quiz.pass = function() {
+      console.log("SINGLE ATTEMPTS", $scope.quiz.numCorrect);
       console.log("NUM QUESTIONS", $scope.quiz.numQuestions);
       var score = Number($scope.quiz.numCorrect) / Number($scope.quiz.numQuestions);
-      console.log(score);
+      console.log("SCORE", score);
       return score > 0.66;
     }
 
